5e705bc3578e87fb5fc6c6be798fd439f2b611cd
CyberpunkNot2077 <155694165+CyberpunkNot2077@users.noreply.github.com>
2024-01-19 12:19:27 -0800

    Update script.js

--- script.js
+++ script.js
@@ -6,15 +6,27 @@ document.addEventListener("DOMContentLoaded", function (event) {
 function welcomeNewUser(){
   document.getElementById("hello").disabled = true;
   let username = document.getElementById("username").value;
+  localStorage.setItem("username", username);
   let passcode = document.getElementById("passcode").value;
+  localStorage.setItem("passcode", passcode);
   let town = document.getElementById("town").value;
+  localStorage.setItem("town", town);
   let state = document.getElementById("state").value;
+  localStorage.setItem("state", state);
 }
 function welcomePastUser(){
   document.getElementById("hello").disabled = true;
+  document.getElementByID("welcome-back").disabled = false;
   let username = document.getElementById("username").value;
   let passcode = document.getElementById("passcode").value;
 }
 function helloThere(){
-  
+  document.getElementById("hello").disabled = true;
+  document.getElementById("welcome-aboard").disabled = false;
+  document.getElementById("welcome-back").disabled = true;
 }
+function goodToSeeYouAgain(){
+  document.getElementById("hello").disabled = true;
+  docu
+}
+
