39d5958c89fa3cbd14475c1c7affd068d344814c
CyberpunkNot2077 <155694165+CyberpunkNot2077@users.noreply.github.com>
2024-01-18 22:58:48 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ document.addEventListener("DOMContentLoaded", function (event) {
   document.getElementById("welcome-back").disabled = true;
 )}
 
-function 
+function welcomeNewUser(){
+  let username = document.getElementById("username").value;
+  let passcode = document.getElementById("passcode").value;
+  let town = document.getElementById("town").value;
+  let state = document.getElementById("state").value;
+}
