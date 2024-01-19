847e657b5a2214203d05a7e88055ebe9947f0830
CyberpunkNot2077 <155694165+CyberpunkNot2077@users.noreply.github.com>
2024-01-19 11:06:26 -0800

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,17 @@ document.addEventListener("DOMContentLoaded", function (event) {
 )}
 
 function welcomeNewUser(){
+  document.getElementById("hello").disabled = true;
   let username = document.getElementById("username").value;
   let passcode = document.getElementById("passcode").value;
   let town = document.getElementById("town").value;
   let state = document.getElementById("state").value;
+}
+function welcomePastUser(){
+  document.getElementById("hello").disabled = true;
+  let username = document.getElementById("username").value;
+  let passcode = document.getElementById("passcode").value;
+}
+function helloThere(){
+  
 }
